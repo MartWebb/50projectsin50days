f15ba294316f3575fd5633245aa80292cde03fe6
Martin <martwebb2@gmail.com>
2021-07-14 18:04:54 -0400

    good cheap fast checkboxes

--- drawing-app/index.js
+++ drawing-app/index.js
@@ -1,38 +1,40 @@
 const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
 const sizeEL = document.getElementById('size');
 const colorEl = document.getElementById('color');
 const clearEl = document.getElementById('clear');
 
-const ctx = canvas.getContext('2d');
-
+console.log(ctx)
 let size = 10;
-let isPressed = false;
-colorEl.value = 'black';
+colorEl.value = '#000000';
 let color = colorEl.value;
 let x;
 let y;
+let isPressed = false;
 
-canvas.addEventListener('mousedown', (e) => {
+canvas.addEventListener('mousedown', (event) => {
     isPressed = true;
 
-    x = e.offsetX;
-    y = e.offsetY;
-})
+    x = event.offsetX;
+    y = event.offsetY;
 
-canvas.addEventListener('mouseup', (e) => {
+    console.log(isPressed, x, y)
+});
+
+canvas.addEventListener('mouseup', (event) => {
     isPressed = false;
 
     x = undefined;
     y = undefined;
-})
+});
 
-canvas.addEventListener('mousemove', (e) => {
-    if(isPressed) {
-        const x2 = e.offsetX;
-        const y2 = e.offsetY;
-
+canvas.addEventListener('mousemove', (event) => {
+    if (isPressed) {
+        const x2 = event.offsetX;
+        const y2 = event.offsetY;
+        console.log( x2, y2)
         drawCircle(x2, y2);
         drawLine(x, y, x2, y2);
 
@@ -42,45 +44,45 @@ canvas.addEventListener('mousemove', (e) => {
 });
 
 function drawCircle(x, y) {
-    ctx.beginPath();
-    ctx.arc(x, y, size, 0, Math.PI * 2);
-    ctx.fillStyle = color;
-    ctx.fill();
+   ctx.beginPath();
+   ctx.arc(x, y, size, 0, Math.PI * 2); // Outer circle
+   ctx.fillStyle = color;
+   ctx.fill();
 }
 
-function drawLine(x1, y1, x2, y2) {
+function drawLine (x1, y1, x2, y2) {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.strokeStyle = color;
     ctx.lineWidth = size * 2;
     ctx.stroke();
+
 }
+
+console.log(colorEl)
 
 function updateSizeOnScreen() {
     sizeEL.innerText = size;
 }
-
-increaseBtn.addEventListener('click', () => {
+increaseBtn.addEventListener('click', (event) => {
     size += 5;
 
-    if(size > 50) {
+    if (size > 50) {
         size = 50;
     }
-
     updateSizeOnScreen();
 });
 
-decreaseBtn.addEventListener('click', () => {
+decreaseBtn.addEventListener('click', (event) => {
     size -= 5;
 
-    if(size < 5) {
-        size = 5
+    if (size < 5) {
+        size = 5;
     }
-
     updateSizeOnScreen();
 });
 
-colorEl.addEventListener('change', (e) => color = e.target.value);
+colorEl.addEventListener('change', (event) => color = event.target.value);
 
-clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height));
+clearEl.addEventListener('click', (event) => ctx.clearRect(0, 0, canvas.width, canvas.height));
