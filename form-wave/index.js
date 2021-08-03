5cff07e7a43a30d46b0dc34757f1e592377e2f64
Martin <martwebb2@gmail.com>
2021-08-03 17:50:46 -0400

    changed form wave

--- form-wave/index.js
+++ form-wave/index.js
@@ -1,8 +1,8 @@
-const labels = document.querySelectorAll('.form-control label');
+ const labels = document.querySelectorAll('.input-container label');
 
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-        .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-        .join('')
-});
+ console.log(labels)
+
+ labels.forEach(label => {
+    label.innerHTML = label.innerText.split('').map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`).join('');
+ });
+ 
